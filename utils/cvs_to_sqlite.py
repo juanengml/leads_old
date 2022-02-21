377ae090bede574b79e12a20780a9de644b472ec
Marcello Sandi Pinheiro <msandipinheiro@gmail.com>
2022-02-21 13:43:54 -0300

    Atualização dos notebooks e requirements-dev.txt

--- utils/cvs_to_sqlite.py
+++ utils/cvs_to_sqlite.py
@@ -4,26 +4,27 @@ import pandas as pd
 import glob
 import os
 
-def cvs_to_sqlite(dbName,path_recebidos,tbName):
+
+def csv_to_sqlite(dbName, path_recebidos, tbName):
     con = sqlite3.connect(dbName)
-    
+
     all_files = glob.glob(path_recebidos + "\*" + ".csv")
-    n=0
-    li=[]    
-    for filename in all_files:  
+    n = 0
+    li = []
+    for filename in all_files:
         try:
-            df = pd.read_csv(filename,sep=';')
+            df = pd.read_csv(filename, sep=';')
             df.to_sql(tbName, con, if_exists='append', index=False)
-            n+=1
+            n += 1
         except:
             li.append(filename)
-    arq_ok=n
+    arq_ok = n
     return arq_ok, li
 
 
 def get_column_names_from_db_table(dbName, table_name):
     con = sqlite3.connect(dbName)
-    sql_cursor= con.cursor()
+    sql_cursor = con.cursor()
 
     table_column_names = 'PRAGMA table_info(' + table_name + ');'
     sql_cursor.execute(table_column_names)
